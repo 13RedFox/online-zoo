3d859034ea545bea2735af2c8acae58f0353f9c6
13redfox <sergei181989@gmail.com>
2021-03-26 20:02:02 +0200

    feat: add preloader block

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // *	Variables:
+const preload = document.querySelector('#preload');
 const header = document.querySelector('.header');
 const burger = document.querySelector('.burger');
 const mobileMenu = document.querySelector('.header__mobile');
@@ -12,41 +13,47 @@ const lightBG = 'light-mode-bg';
 const darkColor = 'dark-mode-col';
 const darkBG = 'dark-mode-bg';
 
+// !	PRELOAD
+
+window.onload = function () {
+  preload.style.display = 'none';
+};
+
 // !	TOGGLE
+
 toggle.addEventListener('change', () => {
+  // *	Header menu
+  for (let i = 0; i < headerMenu.length; i++) {
+    if (toggle.checked) {
+      headerMenu[i].classList.toggle(darkColor);
+    } else {
+      headerMenu[i].classList.toggle(darkColor);
+    }
+  }
 
-	// *	Header menu
-	for (let i = 0; i < headerMenu.length; i++) {
-		if (toggle.checked) {
-			headerMenu[i].classList.toggle(darkColor);
-		} else {
-			headerMenu[i].classList.toggle(darkColor);
-		}
-	}
+  // *	Burger line
+  for (let i = 0; i < burgerLine.length; i++) {
+    if (toggle.checked) {
+      burgerLine[i].classList.toggle(lightBG);
+    } else {
+      burgerLine[i].classList.toggle(lightBG);
+    }
+  }
 
-	// *	Burger line
-	for (let i = 0; i < burgerLine.length; i++) {
-		if (toggle.checked) {
-			burgerLine[i].classList.toggle(lightBG);
-		} else {
-			burgerLine[i].classList.toggle(lightBG);
-		}
-	}
-
-	// *	Header & Header logo
-	if (toggle.checked) {
-		header.classList.toggle(darkBG);
-		mobileMenu.classList.toggle(darkBG);
-		headerLogo.classList.toggle(darkColor);
-	} else {
-		header.classList.toggle(darkBG);
-		mobileMenu.classList.toggle(darkBG);
-		headerLogo.classList.toggle(darkColor);
-	}
-
+  // *	Header & Header logo
+  if (toggle.checked) {
+    header.classList.toggle(darkBG);
+    mobileMenu.classList.toggle(darkBG);
+    headerLogo.classList.toggle(darkColor);
+  } else {
+    header.classList.toggle(darkBG);
+    mobileMenu.classList.toggle(darkBG);
+    headerLogo.classList.toggle(darkColor);
+  }
 });
 
 // !	Burger menu
+
 burger.addEventListener('click', () => {
-	mobileMenu.classList.toggle('header__active');
+  mobileMenu.classList.toggle('header__active');
 });
